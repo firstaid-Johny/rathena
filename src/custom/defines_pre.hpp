3a663bbc9043ffa8ac621b0040c3c4b5a69071b9
Striker <firstaid352@gmail.com>
2026-09-12 09:54:21 +0700

    Update Exe

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -4,6 +4,7 @@
 #ifndef CONFIG_CUSTOM_DEFINES_PRE_HPP
 #define CONFIG_CUSTOM_DEFINES_PRE_HPP
 
+#define PACKETVER 20260219
 /**
  * rAthena configuration file (http://rathena.org)
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
